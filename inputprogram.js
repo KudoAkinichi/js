ca8c5d17e3ab613e14541a4d36828b2101aa3010
KudoAkinichi <arookrishna@gmail.com>
2022-08-21 10:46:54 +0530

    first exp.

---
+++ inputprogram.js
@@ -0,0 +1,6 @@
+const prompt = require('prompt-sync')();
+
+var fname = prompt("Enter your first name:");
+var lname = prompt("Enter your last name:");
+
+console.log("Your full name is",fname,lname);
